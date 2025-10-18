ab69784e4a22af9f2a8c8c576352476fe14b669a
Jack Tsui <jacktsui23@gmail.com>
2025-10-18 14:21:07 -0500

    Finished lagest rectangle historgram problem

--- largestRectangle.py
+++ largestRectangle.py
@@ -3,10 +3,47 @@ from typing import List
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         stack = []
-        stack.append(heights[0])
+        maxArea = 0
 
-        current = 1
-        while stack and stack[-1] > heights[current]:
-            pass
+        for i, h in enumerate(heights):
+            
+            while stack and stack[-1][1] > h:
+                tall = stack[-1][1]
+                stack.pop()
+
+                width = 0
+
+                if not stack: 
+                    width = i
+                else:
+                    width = i - stack[-1][0] - 1
+
+                area = width * tall
+
+                maxArea = max(area, maxArea)
+            
+            stack.append((i,h))
+
+
+        while stack:
+            i, tall = stack.pop()
+            
+            width = 0
+            if not stack:
+                width = len(heights)
+
+            else:
+                width = len(heights) - stack[-1][0] - 1
+
+            area = width * tall
+
+            maxArea = max(maxArea, area)
+
+
+        return maxArea
+
+                        
+
+
                         
 

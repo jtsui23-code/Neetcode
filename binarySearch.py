1166209984a0af0b01a1e51d5fea481b5f480d05
Jack Tsui <jacktsui23@gmail.com>
2025-10-20 21:08:19 -0500

    DId binary search neetcode.

---
+++ binarySearch.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        if len(nums) <= 0:
+            return -1
+
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m =  l + (r - l) // 2
+
+            if target == nums[m]:
+                return m
+            elif target > nums[m]:
+                l = m + 1
+
+            else:
+                r = m -1
+
+        return -1

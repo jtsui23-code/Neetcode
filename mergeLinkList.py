682845f3595b09d52f27c5866210495521909443
Jack Tsui <jacktsui23@gmail.com>
2025-11-10 13:52:15 -0600

    DId Merge Two Sorted Linked Lists

---
+++ mergeLinkList.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        head = ListNode()
+        head1 = list1
+        head2 = list2
+        current = head
+
+        while head1 and head2:
+            
+            if head1.val >= head2.val:
+                temp = head2
+                current.next = temp
+                head2 = head2.next
+
+            elif head1.val < head2.val:
+                temp = head1
+                current.next = temp
+                head1 = head1.next
+
+            current = current.next
+
+        current.next = head1 if head1 else head2
+
+        return head.next
